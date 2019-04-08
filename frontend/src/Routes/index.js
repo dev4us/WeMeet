8cc34772a27cc278bbf886f37709e290a5315dde
dev4us <bronxofsoul@gmail.com>
2019-04-08 15:59:26 +0900

    [FE] init Commit

---
+++ frontend/src/Routes/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./About";
+
+const RouteSet = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={About} />
+    </Switch>
+  </BrowserRouter>
+);
+
+const Routes = () => {
+  return <RouteSet />;
+};
+
+export default Routes;
