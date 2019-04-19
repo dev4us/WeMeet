dd193a5856956c01ced6c4f47261cca471ad2d77
dev4us <bronxofsoul@gmail.com>
2019-04-19 17:42:01 +0900

    [BE] dev GetMeeting (Detail)

---
+++ backend/src/api/Meeting/GetMeeting/GetMeeting.resolvers.ts
@@ -0,0 +1,59 @@
+import { Resolvers } from "../../../../src/types/resolvers";
+import privateResolver from "../../../../src/utils/privateResolvers";
+import {
+  GetMeetingQueryArgs,
+  GetMeetingResponse
+} from "../../../../src/types/graphql";
+import Meeting from "../../../../src/entities/Meeting";
+import { getRepository } from "typeorm";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetMeeting: privateResolver(
+      async (
+        _,
+        args: GetMeetingQueryArgs,
+        { req }
+      ): Promise<GetMeetingResponse> => {
+        const { hashKey } = args;
+        const { user } = req;
+
+        try {
+          const getMeeting = await getRepository(Meeting)
+            .createQueryBuilder("meeting")
+            .innerJoinAndSelect(
+              "meeting.participants",
+              "participants",
+              "participants.id = :userId",
+              { userId: user.id }
+            )
+            .leftJoinAndSelect("meeting.confirmDay", "confirmDay")
+            .where({ hashKey })
+            .getOne();
+
+          if (!getMeeting) {
+            return {
+              ok: false,
+              error: "해당하는 일정이 존재하지 않습니다",
+              Meeting: null
+            };
+          }
+
+          return {
+            ok: true,
+            error: null,
+            Meeting: getMeeting
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            Meeting: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
--- backend/src/types/graphql.d.ts
+++ backend/src/types/graphql.d.ts
@@ -1,22 +1,27 @@
-export const typeDefs = ["type Day {\n  id: Int!\n  pickDate: String!\n  asMeeting: Meeting\n  participants: [User]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  Day: String!\n  GetMeetings(reqType: String!): GetMeetingsResponse!\n  Meeting: String!\n  GetProfile: GetProfileResponse!\n  User: String!\n}\n\ntype CreateMeetingResponse {\n  ok: Boolean!\n  error: String\n  Meeting: Meeting\n}\n\ntype Mutation {\n  CreateMeeting(title: String!, description: String!, thumbnail: String!): CreateMeetingResponse!\n  SignIn(userEmail: String!, userName: String!, profileImage: String): SignInResponse!\n}\n\ntype GetMeetingsResponse {\n  ok: Boolean!\n  error: String\n  Meetings: [Meeting]\n}\n\ntype Meeting {\n  id: Int!\n  title: String!\n  hashKey: String!\n  thumbnail: String\n  description: String!\n  participants: [User]\n  admin: User\n  meetingDays: [Day]\n  confirmDay: Day\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetProfileResponse {\n  ok: Boolean!\n  error: String\n  User: User\n}\n\ntype User {\n  id: Int!\n  userName: String!\n  userEmail: String!\n  profileImage: String\n  meetings: [Meeting]\n  manages: [Meeting]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n  userName: String\n}\n"];
+export const typeDefs = ["type Day {\n  id: Int!\n  pickDate: String!\n  asMeeting: Meeting\n  participants: [User]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  Day: String!\n  GetMeeting(hashKey: String!): GetMeetingResponse!\n  GetMeetings(reqType: String!): GetMeetingsResponse!\n  Meeting: String!\n  GetProfile: GetProfileResponse!\n  User: String!\n}\n\ntype CreateMeetingResponse {\n  ok: Boolean!\n  error: String\n  Meeting: Meeting\n}\n\ntype Mutation {\n  CreateMeeting(title: String!, description: String!, thumbnail: String!): CreateMeetingResponse!\n  SignIn(userEmail: String!, userName: String!, profileImage: String): SignInResponse!\n}\n\ntype GetMeetingResponse {\n  ok: Boolean!\n  error: String\n  Meeting: Meeting\n}\n\ntype GetMeetingsResponse {\n  ok: Boolean!\n  error: String\n  Meetings: [Meeting]\n}\n\ntype Meeting {\n  id: Int!\n  title: String!\n  hashKey: String!\n  thumbnail: String\n  description: String!\n  participants: [User]\n  admin: User\n  meetingDays: [Day]\n  confirmDay: Day\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetProfileResponse {\n  ok: Boolean!\n  error: String\n  User: User\n}\n\ntype User {\n  id: Int!\n  userName: String!\n  userEmail: String!\n  profileImage: String\n  meetings: [Meeting]\n  manages: [Meeting]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n  userName: String\n}\n"];
 /* tslint:disable */
 
 export interface Query {
   Day: string;
+  GetMeeting: GetMeetingResponse;
   GetMeetings: GetMeetingsResponse;
   Meeting: string;
   GetProfile: GetProfileResponse;
   User: string;
 }
 
+export interface GetMeetingQueryArgs {
+  hashKey: string;
+}
+
 export interface GetMeetingsQueryArgs {
   reqType: string;
 }
 
-export interface GetMeetingsResponse {
+export interface GetMeetingResponse {
   ok: boolean;
   error: string | null;
-  Meetings: Array<Meeting> | null;
+  Meeting: Meeting | null;
 }
 
 export interface Meeting {
@@ -51,6 +56,12 @@ export interface Day {
   participants: Array<User> | null;
   createdAt: string;
   updatedAt: string | null;
+}
+
+export interface GetMeetingsResponse {
+  ok: boolean;
+  error: string | null;
+  Meetings: Array<Meeting> | null;
 }
 
 export interface GetProfileResponse {
